a72f0d7c3839c59dbf8caf82367770318115fafd
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-17 00:47:15 +0100

    typo in railtie

--- lib/simple_presenter/railtie.rb
+++ lib/simple_presenter/railtie.rb
@@ -1,6 +1,6 @@
 module SimplePresenter
   class Railtie < Rails::Railtie
-    initialize 'simple_presenter.view_helper' do |app|
+    initializer 'simple_presenter.view_helper' do |app|
       ActiveSupport.on_load :action_controller do
         include SimplePresenter::ViewHelper
       end
