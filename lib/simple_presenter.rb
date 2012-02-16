ed78bfa763db11b90d884cab10147086f40609fe
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-17 00:33:04 +0100

    require files in /lib/simple_presenter

--- lib/simple_presenter.rb
+++ lib/simple_presenter.rb
@@ -1,5 +1,14 @@
-require "simple_presenter/version"
+require 'active_support/core_ext/string'
+require 'active_support/concern'
+
+require 'named_accessors'
+
+require 'simple_presenter/version'
+require 'simple_presenter/base'
+require 'simple_presenter/view_helper'
+
+require 'simple_presenter/railtie' if defined? Rails
+
 
 module SimplePresenter
-  # Your code goes here...
 end
