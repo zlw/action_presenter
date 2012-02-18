4738df0087f7e85fb9393e10dfc588f0973bb15f
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-18 14:23:51 +0100

    refactor defaults/timestamps specs

--- spec/action_presenter/defaults/timestamps_spec.rb
+++ spec/action_presenter/defaults/timestamps_spec.rb
@@ -5,41 +5,42 @@ require 'support/dummy_action_controller'
 describe ActionPresenter::Defaults::Timestamps do
   include ActionControllerHelper
 
-  let(:time) { Time.now }
+  let(:time_now) { Time.now }
+  let(:time) { 'Fri, 17 Feb 2012 01:46:08 +0100' }
 
-  let(:object) { mock(Object, created_at: time, updated_at: time) }
+  let(:object) { mock(Object, created_at: time_now, updated_at: time_now) }
   let(:template) { mock(Object) }
 
   let(:klass) { DummyPresenter.new(object, template) }
 
   it 'should return localized #created_at' do
-    I18n.should_receive(:l).and_return("Fri, 17 Feb 2012 01:46:08 +0100")
-    klass.created_at.should == "Fri, 17 Feb 2012 01:46:08 +0100"
+    I18n.should_receive(:l).and_return time
+    klass.created_at.should == time
   end
 
   it 'should return localized #updated_at' do
-    I18n.should_receive(:l).and_return("Fri, 17 Feb 2012 01:46:08 +0100")
-    klass.updated_at.should == "Fri, 17 Feb 2012 01:46:08 +0100"
+    I18n.should_receive(:l).and_return time
+    klass.updated_at.should == time
   end
 
   context 'in view context' do
     let(:object) { 'foobar' }
-    before { I18n.should_receive(:l).and_return 'Fri, 17 Feb 2012 01:46:08 +0100' }
+    before { I18n.should_receive(:l).and_return time }
 
     it 'should return #created_at' do
-      object.should_receive(:created_at).and_return '2012-02-17 01:46:08 +0100'
+      object.should_receive(:created_at).and_return time
 
       helper.present(object) do |p|
         p.created_at
-      end.should == 'Fri, 17 Feb 2012 01:46:08 +0100'
+      end.should == time
     end
 
     it 'should return #updated_at' do
-      object.should_receive(:updated_at).and_return '2012-02-17 01:46:08 +0100'
+      object.should_receive(:updated_at).and_return time
 
       helper.present(object) do |p|
         p.updated_at
-      end.should == 'Fri, 17 Feb 2012 01:46:08 +0100'
+      end.should == time
     end
   end
 end
