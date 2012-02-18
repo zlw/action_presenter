fad42ab8806d90254bc5656899cf6a9e199c1b63
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-18 13:11:03 +0100

    install generator

---
+++ lib/generators/action_presenter/install/install_generator.rb
@@ -0,0 +1,23 @@
+module ActionPresenter
+  class InstallGenerator < Rails::Generators::Base
+    desc <<-DESC
+    Description:
+      Generate application and test/spec presenter folders
+    DESC
+
+    class_option 'test-framework', type: :string, default: :rspec, aliases: '-t', desc: 'Test framework (RSpec or Test::Unit)'
+
+    def build_app_presenters_folder
+      empty_directory 'app/presenters'
+    end
+
+    def build_test_presenters_folder
+      case options['test-framework']
+        when 'rspec'
+          empty_folder 'spec/presenters'
+        when 'test_unit'
+          empty_folder 'test/unit/presenters'
+      end
+    end
+  end
+end
