4b4f7c3d3444c3dfddf17177530c10ab9d5da4cb
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-18 13:31:33 +0100

    fix rspec integration (RSpec::Rails::RailsExampleGroup was needed)
    and add RSpec::Matches::RenderTemplate

--- lib/action_presenter/rspec_integration.rb
+++ lib/action_presenter/rspec_integration.rb
@@ -1,5 +1,11 @@
 module Draper
   RSpec.configure do |config|
+    config.include RSpec::Rails::RailsExampleGroup, example_group: { file_path: %r{spec/presenters} }
+
+    # Access to view object
     config.include ActionView::TestCase::Behavior, example_group: { file_path: %r{spec/presenters} }
+
+    # Access to #should render_template() etc.
+    config.include RSpec::Rails::Matchers::RenderTemplate, example_group: { file_path: %r{spec/presenters} }
   end
 end
