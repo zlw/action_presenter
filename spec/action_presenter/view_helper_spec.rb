8a7d5c992318d2e849ce697f0b4900140026e316
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-20 22:08:57 +0100

    refactor view_helper_spec

--- spec/action_presenter/view_helper_spec.rb
+++ spec/action_presenter/view_helper_spec.rb
@@ -8,14 +8,14 @@ describe ActionPresenter::ViewHelper do
 
   it 'should call default presenter if presenter class is not given' do
     helper.present('foobar') do |p|
-      p.foobar
-    end.should == 'foo'
+      p.should be_instance_of StringPresenter
+    end
   end
 
   it 'should call given presenter class' do
     helper.present('foobar', FoobarPresenter) do |p|
-      p.foobar
-    end.should == 'bar'
+      p.should be_instance_of FoobarPresenter
+    end
   end
 
   it 'should return nil if no block is given' do
